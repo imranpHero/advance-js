3c0b30c7bf78382d041f8931f833ffe3a4ec7eb8
imranpHero <ik525405@gmail.com>
2020-09-06 00:55:56 +0600

    map4

---
+++ map-filter.js
@@ -0,0 +1,16 @@
+
+numbers = [3, 5, 4, 7];
+
+// function squre (element){
+//   return element * element;
+// }
+
+// numbers.map(function(element){
+//   console.log(element);
+// })
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+const bigger = numbers.filter(x => x > 5);
+console.log(bigger);
